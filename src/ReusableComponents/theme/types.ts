2a6eed5358dd97807ca9080fcd009052685bfeaf
dreplica <adejo.david@aiesec.net>
2020-05-04 21:31:20 +0100

    fixed a single card component

---
+++ src/ReusableComponents/theme/types.ts
@@ -0,0 +1,26 @@
+
+interface COLORS{
+    BG_DARK: string;
+    BG_LIGHT: string;
+    BG_ORANGE: string;
+    FONT_LIGTH: string;
+}
+
+interface FONTS{
+    MEDIUM_STYLE: string;
+    BOLD_STYLE: string;
+    LIGHT_STYLE: string;
+    LARGE_SIZE: string;
+    MEDIUM_SIZE: string;
+    SMALL_SIZE: string;
+}
+
+
+const Colors: COLORS = {
+    BG_DARK: `rgb(24, 17, 17)`,
+    BG_LIGHT: 'white',
+    BG_ORANGE: 'orange',
+    FONT_LIGTH: `rgb(230, 208, 208)`
+}
+
+export default {}
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,11 +2,19 @@ import React, { Fragment } from 'react';
 
 import Logo from './HeadLogo/Logo/index'
 import Navigation from './Navigation';
+import Cards from '../../ReusableComponents/Cards';
 export default function Header() {
     return (
         <Fragment>
             <Logo />
             <Navigation />
+            <Cards
+                image="string"
+                name="hello"
+                price='10'
+                desc="talking"
+                id="2"
+            />
         </Fragment>
     );
 }
