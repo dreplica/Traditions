aae4626d43831594a3aa27a6d49e4fb663da60ee
David <adejo.david@aiesec.net>
2020-05-25 10:44:10 +0100

    fixed category overflow issue

--- src/components/categories/style.ts
+++ src/components/categories/style.ts
@@ -3,16 +3,25 @@ import styled from 'styled-components';
 export const Container = styled.section`
          min-width: 300px;
          max-width: 300px;
+         height:80vh;
          position: relative;
          z-index: 5;
          align-self: flex-start;
          background: grey;
 
          @media (max-width: 999px) {
-           position: absolute;
+           position: fixed;
            left: -300px;
+           /* top:30px; */
+           z-index:15;
          }
        `;
+
+export const Cat = styled.div`
+    width:100%;
+    height:80vh;
+    overflow-y:auto;
+`
 
 export const Ball = styled.div`
     width:auto;
@@ -58,6 +67,7 @@ export const Name = styled.h3`
 export const List = styled.div`
     padding-left:10px;
     display:flex;
+    height:auto;
     flex-direction:column;
     
     a{
