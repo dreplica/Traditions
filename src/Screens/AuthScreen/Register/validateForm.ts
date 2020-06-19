7938038ec1c6f765e12b8f37afb13033b5d2abd8
dreplica <adejo.david@aiesec.net>
2020-06-19 01:19:49 +0100

    commmented registration component out

--- src/Screens/AuthScreen/Register/validateForm.ts
+++ src/Screens/AuthScreen/Register/validateForm.ts
@@ -1,40 +1,42 @@
-import { SIGNUP_FORM } from "../../../ReusableComponents/theme/types";
+// import { SIGNUP_FORM } from "../../../ReusableComponents/theme/types";
 
-type Company = "companyname" | "companydesc" | "logo";
+// type Company = "companyname" | "companydesc" | "logo";
 
-type User =
-  | "firstname"
-  | "lastname"
-  | "username"
-  | "password"
-  | "email"
-  | "phone";
+// type User =
+//   | "firstname"
+//   | "lastname"
+//   | "username"
+//   | "password"
+//   | "email"
+//   | "phone";
 
-export default function validateRegistration(
-  form: SIGNUP_FORM
-): string | boolean {
-  const companyRequiredFields: Company[] = [
-    "companyname",
-    "companydesc",
-    "logo",
-  ];
-  const userRequiredField: User[] = [
-    "firstname",
-    "lastname",
-    "username",
-    "password",
-    "email",
-    "phone",
-  ];
+// export default function validateRegistration(
+//   form: SIGNUP_FORM
+// ): string | boolean {
+//   const companyRequiredFields: Company[] = [
+//     "companyname",
+//     "companydesc",
+//     "logo",
+//   ];
+//   const userRequiredField: User[] = [
+//     "firstname",
+//     "lastname",
+//     "username",
+//     "password",
+//     "email",
+//     "phone",
+//   ];
 
-  const ErrorLogs: string[] = [];
+//   const ErrorLogs: string[] = [];
 
-  (form['admin'] === 1) && companyRequiredFields.map((item) =>
-    form[item] === "" ? ErrorLogs.push(item) : null
-  );
-  userRequiredField.map((item) =>
-    form[item] === "" ? ErrorLogs.push(item) : null
-  );
+//   (form['admin'] === 1) && companyRequiredFields.map((item) =>
+//     form[item] === "" ? ErrorLogs.push(item) : null
+//   );
+//   userRequiredField.map((item) =>
+//     form[item] === "" ? ErrorLogs.push(item) : null
+//   );
 
-  return ErrorLogs.length ? ErrorLogs.join(",") : false;
-}
+//   return ErrorLogs.length ? ErrorLogs.join(",") : false;
+// }
+
+export default {};
