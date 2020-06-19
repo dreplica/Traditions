71ee32998b0f6ebb45e7790a9ca70915289b853f
dreplica <adejo.david@aiesec.net>
2020-06-20 00:57:22 +0100

    worked out that error, found out i cd'ed into the file wrongly, instead of cd Traditions, i did cd traditions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import store from './store/store';
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import Maincomponent from './routes/route';
+import store from './store/store';
 import Header from './components/header';
 import Modal from './components/modal';
 const App = () => {
