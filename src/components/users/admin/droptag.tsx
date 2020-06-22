bdce808eb30b470461563544f581c6e248b06567
dreplica <adejo.david@aiesec.net>
2020-06-22 01:27:21 +0100

    working around modal casing issue

--- src/components/users/admin/droptag.tsx
+++ src/components/users/admin/droptag.tsx
@@ -1,5 +1,5 @@
 import React, { useState,useEffect } from 'react'
-import { DropIT } from '../admin/adminstyle'
+import { DropIT } from './adminstyle'
 
 const DropTag:React.FC<{name:string}> = ({name})=>{
   const [count, setcount] = useState<number>(90)
